5be82edfe6157bc5440d2216f8e1f7e825d28da5
weby-homelab <rekvizitor.ua@gmail.com>
2026-08-22 15:56:24 +0300

    release: prepare GUI 0.7.8 correction

--- src/power_gui/app.py
+++ src/power_gui/app.py
@@ -42,7 +42,7 @@ from .routes import (
     tasks_router,
 )
 
-POWER_VERSION = getattr(power_framework, "__version__", "3.7.1")
+POWER_VERSION = getattr(power_framework, "__version__", "3.7.2")
 APPLICATION_SCHEMA = "power.application.v2"
 ERROR_LOGGER = logging.getLogger("power_gui.errors")
 
